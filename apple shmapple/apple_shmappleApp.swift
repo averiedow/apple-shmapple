d0261d1b4d7e35559e5f826be505f3ce60e8d520
Averie Dow <averie.addington@gmail.com>
2023-06-23 18:20:58 -0600

    Initial Commit

---
+++ apple shmapple/apple_shmappleApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_shmappleApp.swift
+//  apple shmapple
+//
+//  Created by Averie Dow on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct apple_shmappleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
